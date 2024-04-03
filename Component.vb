22217d31f103c64276648a0d487c7bc41e38538a
GSX1250F <150578858+GSX1250F@users.noreply.github.com>
2024-04-03 16:27:36 +0900

    add

---
+++ Component.vb
@@ -0,0 +1,20 @@
+﻿Public Class Component
+    Sub New(ByRef owner As Actor, ByVal updateOrder As Integer)
+        'updateOrderが小さいコンポーネントほど早く更新される
+        mOwner = owner
+        mUpdateOrder = updateOrder
+        mOwner.AddComponent(Me)
+    End Sub
+
+    Public Sub Update(ByVal deltaTime As Double)
+        ' 各コンポーネント更新（オーバーライド可能）
+    End Sub
+
+    Public Sub ProcessInput(ByVal keyState As System.Windows.Forms.KeyEventArgs)
+        ' 各コンポーネント入力処理（オーバーライド可能）
+    End Sub
+
+    Public mOwner As Actor      '所有アクター
+    Public mUpdateOrder As Integer      'コンポーネントの更新順序
+
+End Class
---
+++ SpriteComponent.vb
@@ -0,0 +1,36 @@
+﻿Public Class SpriteComponent
+    Inherits Component
+    Sub New(ByRef owner As Actor, ByVal drawOrder As Integer)
+        MyBase.New(owner, drawOrder)
+        'mTexture = Nothing
+        mDrawOrder = drawOrder
+        mTexWidth = 0
+        mTexHeight = 0
+        mOwner.mGame.AddSprite(Me)
+    End Sub
+
+    Public Sub Draw(ByRef graph)
+        If mOwner.mState <> Actor.State.EPaused Then
+            Dim w = CInt(mTexWidth * mOwner.mScale)
+            Dim h = CInt(mTexHeight * mOwner.mScale)
+            Dim x = CInt(mOwner.mPosition.X - w / 2)
+            Dim y = CInt(mOwner.mPosition.Y - h / 2)
+            Dim bmp As New Bitmap(w, h)
+            bmp = mTexture
+            'mOwner.mRotationに対する回転処理が必要。。。。。
+
+            graph.DrawImage(mTexture, x, y)
+        End If
+
+    End Sub
+    Public Sub SetTexture(tex As Image)
+        mTexture = tex
+        mTexWidth = tex.Width
+        mTexHeight = tex.Height
+    End Sub
+    Public mTexture As Image
+    Public mDrawOrder As Integer
+    Public mTexWidth As Integer
+    Public mTexHeight As Integer
+
+End Class
